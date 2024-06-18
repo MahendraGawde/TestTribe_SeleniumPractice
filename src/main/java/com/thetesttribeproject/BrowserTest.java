abb741ae960fdf0da6d45728a0cd1bc05ea84276
Mahendra <you@example.com>
2024-06-18 18:55:51 +0530

    Selenium Test Browser first commit

--- src/main/java/com/thetesttribeproject/BrowserTest.java
+++ src/main/java/com/thetesttribeproject/BrowserTest.java
@@ -2,10 +2,18 @@ package com.thetesttribeproject;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserTest {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+    public static void main(String[] args) throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        Thread.sleep(3000);
         driver.quit();
         System.out.println("test passed");
     }
