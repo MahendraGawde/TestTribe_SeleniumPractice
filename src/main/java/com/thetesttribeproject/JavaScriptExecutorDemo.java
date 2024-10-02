a70012da8f84f26cd38b2ccb24df076caa509670
mahendragawde <mahi.gawde011@gmail.com>
2024-10-03 02:41:13 +0530

    JavaScriptExecutorDemo code

--- src/main/java/com/thetesttribeproject/JavaScriptExecutorDemo.java
+++ src/main/java/com/thetesttribeproject/JavaScriptExecutorDemo.java
@@ -7,16 +7,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutorDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://etmoney.com");
         JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        WebElement signUpBtn = driver.findElement(By.xpath("//button[@id ='signUpBtn']"));
-
-        js.executeScript("arguments[0].scrollIntoView()",signUpBtn);
+        WebElement signUpBtn = driver.findElement(By.xpath("//button[@class = 'ctaBtn']"));
+        js.executeScript("window.scrollBy(0,5000)","");
+//        js.executeScript("arguments[0].scrollIntoView()",signUpBtn);
+        Thread.sleep(1000);
+        js.executeScript("arguments[0].click()",signUpBtn);
         System.out.println("Offset of signup webelement : " +js.executeScript("return window.pageYOffset;"));
+        signUpBtn.click();
         driver.quit();
     }
 }
