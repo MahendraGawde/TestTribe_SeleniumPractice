e6178fa75f30abe99fe7573c6936d11ffb620fb3
mahendragawde <mahi.gawde011@gmail.com>
2024-12-06 19:29:07 +0530

    updated code JSExcutor

--- src/main/java/com/thetesttribeproject/EtMoneyStaleElementExample.java
+++ src/main/java/com/thetesttribeproject/EtMoneyStaleElementExample.java
@@ -4,18 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import java.time.Duration;
 
-public class EtMoney {
+public class EtMoneyStaleElementExample {
     public static void main(String[] args) throws InterruptedException {
         // Clicking on Menu and submenu 2nd and then again find menu and other submenu
-        ChromeOptions options = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(options);
-
-
+        WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://etmoney.com");
         driver.manage().window().maximize();
@@ -30,11 +26,11 @@ public class EtMoney {
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
         Thread.sleep(2000);
-
-        WebElement mfElement1 = driver.findElement(By.xpath("//span[@class = 'header-nav-link']"));
+        //Stale element exception occurred if not find same element again after redirection of one menu.
+        mfElement = driver.findElement(By.xpath("//span[@class = 'header-nav-link']"));
         WebElement allAboutMflnk = driver.findElement(By.xpath("//strong[normalize-space()='All about Mutual Funds']"));
 
-        act.moveToElement(mfElement1).moveToElement(allAboutMflnk).click().perform();
+        act.moveToElement(mfElement).moveToElement(allAboutMflnk).click().perform();
 
 
         Assert.assertTrue(driver.getTitle().contains("Invest Online in Mutual Funds in India"));
--- src/main/java/com/thetesttribeproject/JavaScriptExecutorDemo.java
+++ src/main/java/com/thetesttribeproject/JavaScriptExecutorDemo.java
@@ -8,10 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JavaScriptExecutorDemo {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://etmoney.com");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+        JavascriptExecutor js = driver;
 
         WebElement signUpBtn = driver.findElement(By.xpath("//button[@class = 'ctaBtn']"));
         //Scroll till specific level of page
